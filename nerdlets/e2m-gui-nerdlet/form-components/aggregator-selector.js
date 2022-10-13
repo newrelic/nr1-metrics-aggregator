5dd9c34beddd8b1f087147276d8800d7afb4a5e4
Jake Neal <jneal@newrelic.com>
2022-10-13 11:18:39 -0700

    update version and lint fix

--- nerdlets/e2m-gui-nerdlet/form-components/aggregator-selector.js
+++ nerdlets/e2m-gui-nerdlet/form-components/aggregator-selector.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 
 import { RadioGroup, Radio } from 'nr1';
 
-const AggregatorSelector = ({ setAggregator, selectedEventType, selectedAggregator }) => {
+const AggregatorSelector = ({
+  setAggregator,
+  selectedEventType,
+  selectedAggregator
+}) => {
   /* TODO: Pull the disabledOrNotObj out to AddRule Component */
   const disabledOrNotObj = !selectedEventType ? { disabled: true } : {};
   return (
@@ -12,21 +16,21 @@ const AggregatorSelector = ({ setAggregator, selectedEventType, selectedAggregat
         {...disabledOrNotObj}
         label="Summary--use if the query's function is min, max, sum, count, or average"
         value="summary"
-        checked={selectedAggregator == 'summary'}
+        checked={selectedAggregator === 'summary'}
         onClick={() => setAggregator('summary')}
       />
       <Radio
         {...disabledOrNotObj}
         label="Distribution--use if the query's function is percentile or histogram"
         value="distribution"
-        checked={selectedAggregator == 'distribution'}
+        checked={selectedAggregator === 'distribution'}
         onClick={() => setAggregator('distribution')}
       />
       <Radio
         {...disabledOrNotObj}
         label="Unique Count--use if the query's function is unique count"
         value="uniqueCount"
-        checked={selectedAggregator == 'uniqueCount'}
+        checked={selectedAggregator === 'uniqueCount'}
         onClick={() => setAggregator('uniqueCount')}
       />
     </RadioGroup>
